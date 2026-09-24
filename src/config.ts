32e0f60f99f8129e708ee45bfbfcf9747b265220
Svante Bengtson <svante@swantzter.se>
2026-09-24 21:40:48 +0200

    use separate firebase app for admin site

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,12 @@ import { initializeApp } from 'firebase/app'
 import type { Router } from 'vue-router'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD07mROu__kGOuJ-0MyjtjS6R5-DiTfUpM',
-  authDomain: 'the-tricktionary.com',
-  projectId: 'project-5641153190345267944',
-  messagingSenderId: '1048157266079',
-  appId: '1:1048157266079:web:a8ae83f6f16d7436',
-  measurementId: 'G-G282NYD80K'
+  apiKey: "AIzaSyDpxeEGAi9KSSR3vuCq27GwUYiYXSGiNR8",
+  authDomain: "the-tricktionary.com",
+  projectId: "project-5641153190345267944",
+  messagingSenderId: "1048157266079",
+  appId: "1:1048157266079:web:7e27b88b92262698ae02aa",
+  measurementId: "G-G29EYN86T1"
 }
 
 initializeApp(firebaseConfig)
